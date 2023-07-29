d457bbe8f2a2b5ca659fea050eabf0f086bf605a
GhostDaGhost <53021768+GhostDaGhost@users.noreply.github.com>
2023-07-29 08:10:41 -0700

    modify(weatherapp): Work Commit

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -28,6 +28,11 @@
             Credits
         },
         methods: {
+            /**
+                * A function that is triggered when the API request returns a successful callback and provides climate data of the inputted location.
+                * @param newClimateData `any` - The data of the climate within the inputted location.
+                * @returns `void` - This returns nothing.
+            */
             refreshWithNewClimateData (newClimateData: any): void {
                 this.currentClimate = newClimateData;
                 this.weatherIcon = newClimateData.weather[0].icon + '@2x.png';
@@ -41,15 +46,29 @@
                 this.currentPressureInHg = this.getPressureInHg(newClimateData.main.pressure);
             },
 
+            /**
+                * A function that is triggered to hide the error alert box.
+                * @returns `void` - This returns nothing.
+            */
             hideErrorMessage (): void {
                 this.showError = false;
             },
 
+            /**
+                * A function that is triggered when an error with the API request occurs to show an alert box with a customized message.
+                * @param newErrorText `string` - The text to show in the error alert box.
+                * @returns `void` - This returns nothing.
+            */
             displayErrorMessage (newErrorText: string = 'Error 404'): void {
                 this.errorText = newErrorText;
                 this.showError = true;
             },
 
+            /**
+                * A function that is triggered when the user presses 'Enter' on the search input.
+                * @param event `any` - The DOM event from the input trigger.
+                * @returns `void` - This returns nothing.
+            */
             async searchCityForClimateData (event: any): Promise<void> {
                 if (event['key'] === 'Enter') {
                     const spinner: any = document.querySelector('.spinner_container');
@@ -76,6 +95,11 @@
                 }
             },
 
+            /**
+                * A function that is triggered when new climate data is received and the time is needed to be converted to a standard display.
+                * @param locationTimezone `number` - The timezone of the location relaying the new climate data.
+                * @returns `string` - This returns the standard display of the location's time. (Ex: Tuesday 7/29/2023, 7:01:40 AM)
+            */
             formatTimestamp (locationTimezone: number): string {
                 const currentDate: Date = new Date();
                 const locationDate: Date = new Date((currentDate.getTime() + currentDate.getTimezoneOffset() * 60000) + (1000 * locationTimezone));
@@ -83,19 +107,39 @@
                 return `${this.days[locationDate.getDay()]} ${locationDate.toLocaleString()}`;
             },
 
+            /**
+                * A function that is triggered when new climate data is received and a temperature unit is needed to be converted to fahrenheit. 
+                * @param kelvinTemperature `number` - The kelvin temperature of the unit provided.
+                * @returns `string` - This returns the fahrenheit temperature number of what was previously the kelvin temperature number.
+            */
             getTemperatureInFahrenheit (kelvinTemperature: number): string {
                 const fahrenheitTemperature: number = ((Math.round(kelvinTemperature) - 273.15) * 9 / 5) + 32;
                 return fahrenheitTemperature.toFixed(0);
             },
 
+            /**
+                * A function that is triggered when new climate data is received and the wind is needed to be converted to miles per hour (MPH) rather than meters per second. 
+                * @param metersPerSecond `number` - The meters per second provided.
+                * @returns `string` - This returns the miles per hour of the meters per second provided.
+            */
             getWindInMilesPerHour (metersPerSecond: number): string {
                 return (metersPerSecond * 2.23694).toFixed(0);
             },
 
+            /**
+                * A function that is triggered when new climate data is received and the wind degree is needed to be converted to a compass point. 
+                * @param windDegrees `number` - The original unit for the wind degree provided.
+                * @returns `string` - This returns the compass point of the converted wind degree.
+            */
             getWindDegreeInCompassPoint (windDegrees: number): string {
                 return this.compassPoints[Number((windDegrees / 22.5).toFixed(0))];
             },
 
+            /**
+                * A function that is triggered when new climate data is received and the pressure in millibar is needed to be converted to mercury.
+                * @param windDegrees `number` - The original unit for the pressure provided. Usually in millibar.
+                * @returns `string` - This returns the mercury value of the pressure in millibar.
+            */
             getPressureInHg (pressureInMillibar: number): string {
                 return (pressureInMillibar / 33.864).toFixed(2);
             }
@@ -173,6 +217,28 @@
                             Feels Like:
                             <br>
                             <span class="detail">{{ currentFahrenheitFeelsLike }}F</span>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="climatedetails_container" style="margin-top: 13.5px;" v-if="currentClimate.weather !== undefined">
+                <table class="climatedetails">
+                    <tr>
+                        <td>
+                            Sunrise:
+                            <br>
+                            <span class="detail">
+                                {{ new Date(currentClimate.sys.sunrise * 1000).toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3") }}
+                            </span>
+                        </td>
+
+                        <td>
+                            Sunset:
+                            <br>
+                            <span class="detail">
+                                {{ new Date(currentClimate.sys.sunset * 1000).toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3") }}
+                            </span>
                         </td>
                     </tr>
                 </table>
